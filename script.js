a5b9a822c89e8f8dec8233fb94517fb005f80ab5
Zein <senin.email@example.com>
2025-10-21 11:29:22 +0300

    !zooming2

--- script.js
+++ script.js
@@ -452,3 +452,14 @@ function GameOver(){
     alert('game over')
     startGame();
 }
+
+
+let lastTouchEnd = 0;
+
+document.addEventListener('touchend', function (event) {
+  const now = Date.now();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault(); 
+  }
+  lastTouchEnd = now;
+}, { passive: false });
